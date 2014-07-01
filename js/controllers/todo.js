f09be6c68e6014fda3a235b6c63925c6fa94ec45
Michael Gimbel <gimbel0893@gmail.com>
2014-07-01 14:15:42 -0400

    initial commit

---
+++ js/controllers/todo.js
@@ -0,0 +1,43 @@
+Todos.TodosController = Ember.ArrayController.extend({
+    actions: {
+        createTodo: function(){
+            var title = this.get('newTitle');
+            if(!title)
+                return false;
+            if(!title.trim())
+                return;
+
+            var todo = this.store.createRecord('todo', {
+                title: title,
+                isCompleted: false
+            });
+
+            this.set('newTitle', '');
+
+            todo.save();
+        }
+    },
+
+    remaining: function(){
+        return this.filterBy('isCompleted', false).get('length');
+    }.property('@each.isCompleted'),
+
+    inflection: function(){
+        var remaining = this.get('remaining');
+        return remaining === 1 ? 'todo' : 'todos';
+    }.property('remaining')
+});
+
+Todos.TodoController = Ember.ObjectController.extend({
+    isCompleted: function(key, value){
+        var model = this.get('model');
+    
+        if(value === undefined)
+            return model.get('isCompleted');
+        else {
+            model.set('isCompleted', value);
+            model.save();
+            return value;
+        }
+    }.property('model.isCompleted')
+});
